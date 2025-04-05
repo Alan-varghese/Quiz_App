a62b867599c5087b322fbd1621fbdcf463179fc9
Alan Varghese <alanvz757@gmail.com>
2025-04-05 16:18:50 +0400

    Updated

---
+++ lib/data/question.dart
@@ -0,0 +1,34 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestions('Which is the Longest river in India?', [
+    'Ganga',
+    'Bhramaputhra',
+    'Yamuna',
+    'Godhavari',
+  ]),
+  QuizQuestions('Which Indian city is known as the "Pink City"?', [
+    'Jaipur',
+    'Jodhpur',
+    'Bikaner',
+    'Udaipur',
+  ]),
+  QuizQuestions('Which Indian state has the largest population?', [
+    'Uttar Pradesh',
+    'Maharashtra',
+    'Telengana',
+    'Madhya Pradesh',
+  ]),
+  QuizQuestions('Who was the first Indian to travel in space?', [
+    'Rakesh Sharma',
+    'Kalpana Chawla',
+    'Sunitha Williams',
+    'Vikram Sarabhai',
+  ]),
+  QuizQuestions('Which is the smallest state in India by area?', [
+    'Kerala',
+    'Karantaka',
+    'Maharashtra',
+    'West Bengal',
+  ]),
+];
